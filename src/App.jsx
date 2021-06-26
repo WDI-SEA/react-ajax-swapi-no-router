3ffaed79863e2c53d2ab299a9c03bf1b49ee3f6e
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-25 19:00:50 -0500

    Initial commit, Starships component linked.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+import Starships from './Starships'
+
+const App = () => {
+    return <Starships />
+}
+export default App;
---
+++ src/Starships.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+const Starships = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+export default Starships;
