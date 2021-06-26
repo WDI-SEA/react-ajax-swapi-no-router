c68c4452f1053b2b4b1677b9ac8910375dd8ed30
funbunch <shannan_bunch@hotmail.com>
2021-06-26 13:07:05 -0700

    intiial setup test of swapi

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Starships from './Starships'
+
+function App() {
+  return (
+    <div className="App">
+      <Starships />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Starships.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+
+export default function Starships() {
+
+  // const [ships, setShips] = useState([])
+
+  return (
+      <div>
+        Hello from the galaxy. 
+      </div>
+
+  )
+
+}
