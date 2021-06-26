11af83f38cb602b2e5dec01df38d19b2b8428ecf
Brian Young <berkshirehunt@gmail.com>
2021-06-26 14:34:33 -0700

    create

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import Starship from './Components/Starship';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+<Starship />
+    </div>
+  )
+}
+
+export default App;
