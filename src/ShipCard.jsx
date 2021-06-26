58dcf99bab86c2105bf8b702962d50ed7240f255
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-26 16:30:41 -0500

    Set ship variable state, processed data, added ShipCard component.

---
+++ src/ShipCard.jsx
@@ -0,0 +1,8 @@
+const ShipCard = () => {
+    return (
+        <div>
+            ShipCard Component
+        </div>
+    )
+}
+export default ShipCard
--- src/Starships.jsx
+++ src/Starships.jsx
@@ -1,9 +1,34 @@
 import { useState, useEffect } from "react";
+import ShipCard from './ShipCard.jsx'
 
 const Starships = () => {
+    const [ships, setShips] = useState([])
+
+    useEffect(() => {
+        Promise.all([
+            fetch('https://swapi.dev/api/starships/?page=1'),
+            fetch('https://swapi.dev/api/starships/?page=2'),
+            fetch('https://swapi.dev/api/starships/?page=3'),
+            fetch('https://swapi.dev/api/starships/?page=4')
+        ]).then(resPlural => {
+            return Promise.all(resPlural.map(resSing =>
+            {
+                return resSing.json()
+            }))
+        }).then(data => {
+            let shipData = data.map(res => {
+                return res.results
+            })
+            setShips([].concat.apply([], shipData))
+            console.log(ships)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [ships])
+
     return (
         <div>
-            Hello
+            <ShipCard />
         </div>
     )
 }
