dd4bdc43aff494ce22454138e05ab34ccaa8cad0
Brian Young <berkshirehunt@gmail.com>
2021-06-26 14:50:59 -0700

    font + CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+    <h1>SWAPI: Starships</h1>
 <Starship />
     </div>
   )
---
+++ src/Components/Starship.jsx
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Starship = () => {
+
+    // Hold ship data in state
+    const [ships, setShips] = useState([])
+
+    const getAPI = async () => {
+    const response = await axios('https://swapi.dev/api/starships/')
+    // console.log('Ships: ' + ships.data)
+    // console.log(' results: ' + ships.data.results[0].name)
+    // const shipsJson = ships.json()
+    // console.log('ShipsJason: ' + shipsJson.results)
+    const shipsArray = response.data.results
+
+    const mapShips = shipsArray.map((ship, index) => {
+        return (
+            <>
+            <div class="container">
+                <div class="starshipItem" key={index}>
+                    Name: {ship.name}
+                </div>
+            </div>
+            <div class="container">
+                <div class="starshipItem" key={index}>
+                    Starship Class: {ship.starship_class}
+                </div>
+            </div>
+            <div class="container">
+                <div class="starshipItem" key={index}>
+                    Manufacturer: {ship.manufacturer}
+                </div>
+            </div>
+            <div class="container">
+                <div class="starshipItem" key={index}>
+                    Max. Speed: {ship.max_atmosphering_speed}
+                </div>
+                <hr/>
+            </div>
+            </>
+                )
+            })
+    setShips(mapShips)
+    }
+
+    useEffect(() => {
+        getAPI()
+    }, [])
+
+
+    return (
+<>
+        {ships}
+</>
+    )
+}
+
+export default Starship
