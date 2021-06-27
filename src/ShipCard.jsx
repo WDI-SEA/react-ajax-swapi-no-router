b3257d4ca4c041cdf694f006fe920f22afaa13ec
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-27 15:36:42 -0500

    Refactored ShipData to return all ship info directly into ShipCard.jsx

--- src/ShipCard.jsx
+++ src/ShipCard.jsx
@@ -4,31 +4,54 @@ import Pilot from './Pilot'
 const ShipCard = (props) => {
     const [pilots, setPilots] = useState([])
 
-    // useEffect(() => {
-    //     Promise.all([
-    //         props.ships.forEach(pilot => {
-    //             fetch(pilot)
-    //         })
-    //     ]).then(resPlural => {
-    //         return Promise.all(resPlural.map(resSing => {
-    //             return resSing.json()
-    //         }))
-    //     }).then(data => {
-    //         let pilotData = data.map(pilot => {
-    //             return pilot.name
-    //         })
-    //         setPilots(pilotData)
-    //         console.log(pilots)
-    //     }).catch(err => {
-    //         console.log(err)
-    //     })
-    // }, [pilots])
-
     return (
-        <>
-            {props.ships}
-            <Pilot />
-        </>
+        <div className="ship-sing-box">
+            <table>
+                <tbody>
+                    <tr className="capitalize">
+                        <td colspan="2">
+                            <h2>{props.ship.name}</h2>
+                        </td>
+                    </tr>
+                    <tr className="capitalize">
+                        <th>Class</th>
+                        <td>{props.ship.starship_class}</td>
+                    </tr>
+                    <tr>
+                        <th>Cargo Capacity (kg)</th>
+                        <td>{props.ship.cargo_capacity.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                    </tr>
+                    <tr>
+                        <th>Price (₹)</th>
+                        <td>{props.ship.cost_in_credits.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                    </tr>
+                    <tr>
+                        <th>Crew Cap. </th>
+                        <td>{props.ship.crew}</td>
+                    </tr>
+                    <tr>
+                        <th>Passenger Cap. </th>
+                        <td>{props.ship.passengers}</td>
+                    </tr>
+                    <tr>
+                        <th>Hyperdrive Rating</th>
+                        <td>{props.ship.hyperdrive_rating}</td>
+                    </tr>
+                    <tr>
+                        <th>Length (m)</th>
+                        <td>{props.ship["length"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                    </tr>
+                    <tr>
+                        <th>Manufacturer</th>
+                        <td>{props.ship.manufacturer}</td>
+                    </tr>
+                    <tr>
+                        <th>Max Atmo Speed (kph)</th>
+                        <td>{props.ship.max_atmosphering_speed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 export default ShipCard
--- src/Starships.jsx
+++ src/Starships.jsx
@@ -24,61 +24,17 @@ const Starships = () => {
         })
     }, [ships])
 
-    let shipInfo = ships.map(e => {
+    let shipInfo = ships.map(ship => {
         return (
-            <div className="ship-sing-box">
-                <table>
-                    <tbody>
-                        <tr className="capitalize">
-                            <td colspan="2">
-                                <h2>{e.name}</h2>
-                            </td>
-                        </tr>
-                        <tr className="capitalize">
-                            <th>Class</th>
-                            <td>{e.starship_class}</td>
-                        </tr>
-                        <tr>
-                            <th>Cargo Capacity (kg)</th>
-                            <td>{e.cargo_capacity.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                        </tr>
-                        <tr>
-                            <th>Price (₹)</th>
-                            <td>{e.cost_in_credits.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                        </tr>
-                        <tr>
-                            <th>Crew Cap. </th>
-                            <td>{e.crew}</td>
-                        </tr>
-                        <tr>
-                            <th>Passenger Cap. </th>
-                            <td>{e.passengers}</td>
-                        </tr>
-                        <tr>
-                            <th>Hyperdrive Rating</th>
-                            <td>{e.hyperdrive_rating}</td>
-                        </tr>
-                        <tr>
-                            <th>Length (m)</th>
-                            <td>{e["length"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                        </tr>
-                        <tr>
-                            <th>Manufacturer</th>
-                            <td>{e.manufacturer}</td>
-                        </tr>
-                        <tr>
-                            <th>Max Atmo Speed (kph)</th>
-                            <td>{e.max_atmosphering_speed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <ShipCard 
+                ship={ship}
+            />
         )
     })
 
     return (
         <div className="ship-plural-box">
-            <ShipCard ships={shipInfo}/>
+            {shipInfo}
         </div>
     )
 }
