976be1db9806849fc731214616a7877cfe402226
funbunch <shannan_bunch@hotmail.com>
2021-06-27 16:39:40 -0700

    Completed rendering. And added some styling.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Starships from './Starships'
 
 function App() {
   return (
-    <div className="App">
-      <Starships />
+    <div className="bg-dark">
+      <div className="App container py-4">
+        <div className="row g-3">
+          <h1>Starships</h1>
+          <Starships />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Starships.js
+++ src/Starships.js
@@ -6,6 +6,7 @@ export default function Starships() {
 
   const [ships, setShips] = useState([])
 
+
   useEffect(() => {
     // hit the API endpoint - ex Fetch
     fetch('https://swapi.dev/api/starships/')
@@ -16,26 +17,40 @@ export default function Starships() {
         // const shipResults = shipData.results.map(shipData => {
         //   return shipData.name 
         // })
+        //set state to ship
         setShips(shipData.results) 
+        //ships.push(shipData.results)
       })
       .catch(err => console.log(err))
   }, [])
-   const printShips = ships.map((ship, index) => {
-   
-      return (
-        <div key={index}>
-          <h2>{ship.name}</h2>
-          <p>{ship.manufacturer}</p>
+
+  
+  const printShips = ships.map((ship, index) => {
+    return (
+      <div className="col-sm-1 col-md-6 col-lg-4" key={index}>
+        <div className="card h-100">
+          <h4 className="card-header h-100">{ship.name}</h4>
+          <div className="card-body">
+            <p className="card-text">Length: {ship.length}</p>
+            <p className="card-text">Max speed: {ship.max_atmosphering_speed}</p>
+            <p className="card-text">Crew: {ship.crew}</p>
+            <p className="card-text">No. of passengers: {ship.passengers}</p>
+            {/* {ship.pilots ? 
+            ship.pilots.map((pilot) => {
+              return  (<ul>
+                <li>{pilot}</li>
+              </ul>)
+            }): <p>No pilots</p> } */}
         </div>
-      )
-    }) 
-
-   
-  return (
-      <div>
-       {printShips}
       </div>
+    </div>
+  )
+}) 
 
+  return (
+    <>
+      {printShips}
+    </>
   )
 
 }
