fdc23340bb6a1a19db6c2058d4478e5eb2f3e066
funbunch <shannan_bunch@hotmail.com>
2021-06-26 20:14:24 -0700

    Completed rendering ships with fetch

--- src/Starships.js
+++ src/Starships.js
@@ -4,11 +4,36 @@ import { useEffect, useState } from 'react'
 
 export default function Starships() {
 
-  // const [ships, setShips] = useState([])
+  const [ships, setShips] = useState([])
 
+  useEffect(() => {
+    // hit the API endpoint - ex Fetch
+    fetch('https://swapi.dev/api/starships/')
+      .then(response => response.json())
+      .then(shipData => {
+        //set state to ship
+        // console.log(shipData.results[0].name)
+        // const shipResults = shipData.results.map(shipData => {
+        //   return shipData.name 
+        // })
+        setShips(shipData.results) 
+      })
+      .catch(err => console.log(err))
+  }, [])
+   const printShips = ships.map((ship, index) => {
+   
+      return (
+        <div key={index}>
+          <h2>{ship.name}</h2>
+          <p>{ship.manufacturer}</p>
+        </div>
+      )
+    }) 
+
+   
   return (
       <div>
-        Hello from the galaxy. 
+       {printShips}
       </div>
 
   )
