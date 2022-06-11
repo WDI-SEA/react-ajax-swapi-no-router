b1c1b6656116aeff7de88f7a64c2a38011f6aed8
marzo20 <myjune91@gmail.com>
2022-06-11 16:32:24 -0700

    page added

--- src/components/Starships.js
+++ src/components/Starships.js
@@ -28,56 +28,76 @@ export default class Starships extends Component {
         }
     }
     // handle change when opion selected
+    handlePage = async (event) => {
+        try {
+            const response = await axios.get(`https://swapi.dev/api/starships/?page=${event.target.value}`)
+            this.setState({ starshipData: response.data.results })
+
+
+        } catch (err) {
+            console.warn('page select err', err)
+
+        }
+    }
     handleChange = (event) => {
         console.log(event.target.value)
         this.setState({
-                        index: event.target.key,
-                        url: event.target.value
+            index: event.target.key,
+            url: event.target.value
         })
     }
     // handle submit
     handleSubmit = async (event) => {
-        try{
+        try {
             event.preventDefault()
-            const url= this.state.url
-            const res= await axios.get(url)
+            const url = this.state.url
+            const res = await axios.get(url)
             console.log(res)
             this.setState({
                 name: res.data.name,
-                manufacturer: res.data.manufacturer ,
+                manufacturer: res.data.manufacturer,
                 crew: res.data.crew,
                 passengers: res.data.passengers,
                 class: res.data.starship_class,
                 maxSpeed: res.data.max_atmosphering_speed,
                 responded: true
             })
-        }catch(err){
-            console.warn('submit error',err)
+        } catch (err) {
+            console.warn('submit error', err)
         }
     }
 
     render() {
         const optionContainer = (
             <>
+                <option selected disabled hidden>Choose Starship</option>
                 {this.state.starshipData.map((starship, index) => {
-                   return <option key={`starship${index}`} value={starship.url}>{starship.name}</option>
-                }) }
+                    return <option key={`starship${index}`} value={starship.url}>{starship.name}</option>
+                })}
             </>
         )
         return (
             <div>
                 <h2>Starships</h2>
                 <form onSubmit={this.handleSubmit}>
+                    <select name="page" onChange={this.handlePage}>
+                        <option value='1'>Page 1</option>
+                        <option value='2'>Page 2</option>
+                        <option value='3'>Page 3</option>
+                        <option value='4'>Page 4</option>
+
+
+                    </select>
                     <select onChange={this.handleChange}>
-                        <option>Choose Starship</option>
+
                         {optionContainer}
                     </select>
                     <button
                         type="submit"
-                        
+
                     >Go</button>
                 </form>
-                {this.state.responded && <Starship state={this.state}/>}
+                {this.state.responded && <Starship state={this.state} />}
             </div>
         )
     }
