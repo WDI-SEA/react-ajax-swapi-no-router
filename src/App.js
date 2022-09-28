32ad2672857c85e7df82864c296bb4fc9a4ba4fd
elek8871 <elekmichelle@gmail.com>
2022-09-27 19:49:54 -0600

    adds framework

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import StarShips from "./StarShips"
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <StarShips />
+      </div>
+    )
+  }
+}
---
+++ src/StarShips.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+
+export default class StarShips extends Component{
+    state={
+        starship:" "
+    }
+    render(){
+        return(
+            <div>
+                <h1> StarShips are meant to fly </h1>
+            </div>
+        )
+    }
+}
