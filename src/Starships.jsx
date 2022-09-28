a7eee3b6f197693586943458357c4365f0a74955
average-alien <allentang113@gmail.com>
2022-09-27 17:00:52 -0700

    stubs out components

---
+++ src/Starships.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Starships extends Component {
+    render() {
+        return(
+            <div>
+                It's a trap!
+            </div>
+        )
+    }
+}
