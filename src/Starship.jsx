7021abcba458d66ac8f4105f6b70a4ca4e4f160d
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 11:42:27 -0500

    Add cost prop

--- src/Starship.jsx
+++ src/Starship.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Starship extends Component {
+
+    state = {
+        pilots: []
+    }
+
+    // componentDidMount() {
+    //     axios.get('http://swapi.dev/api/starships/')
+    //         .then(response => {
+    //             this.setState({ pilots:  })
+    //         })
+    // }
+
     render() {
         console.log(this.props.starship)
         return (
             <div>
                 <h2>{this.props.starship.name}</h2>
                 <p>Class: {this.props.starship.starship_class}</p>
+                <p>Cost: {this.props.starship.cost_in_credits}</p>
                 <p>Manufacturer: {this.props.starship.manufacturer}</p>
                 <p>Crew: {this.props.starship.crew}</p>
                 <p>Passengers: {this.props.starship.passengers}</p>
