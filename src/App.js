85fa697038acf09d7e6183e00f7885812312440e
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-09-28 13:04:39 -0400

    deliverable

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import StarshipList from './StarshipList'
+
+export default class App extends Component {
+  render() {
+    return(
+      <>
+      <StarshipList/>
+      </>
+    )
+  }
+}
