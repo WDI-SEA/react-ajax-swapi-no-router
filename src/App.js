1d19d818f88b8f5b3155b873d7b64a7b089ad56b
devin-lynch <devin.l.lynch@gmail.com>
2022-09-27 20:14:13 -0500

    Add state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Starships from './Starships'
 import axios from 'axios'
 
 export default class App extends Component {
+
+  state = {
+    starships: []
+  }
+
+  // runs only once -- when the component is first being created and mounted on the virtual dom
+  commponentDidMount() {
+
+  }
+
   render() {
     return (
       <div>
