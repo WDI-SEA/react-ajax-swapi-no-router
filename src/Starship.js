1ffa28f2befd135779d7c3e291d17913c79a2137
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-28 10:04:38 -0700

    coded along in class. not the biggest fan of react

--- src/Starship.js
+++ src/Starship.js
@@ -1,6 +1,45 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import Starships from "./Starships";
+import axios from "axios";
 
 export default class Starship extends Component {
 
+
+
+    state = {
+        pilots: []
+    }
+
+    componentDidMount = async () => {
+        try {
+            const data = this.props.pilots.map((url) => axios.get(url))
+            const promisedData = await Promise.all(data)
+            const shallowState = []
+            promisedData.forEach(pilot => {
+                shallowState.push({
+                    name: pilot.data.name,
+                    url: pilot.data.url
+                })
+            })
+            this.setState({
+                pilots: shallowState
+            })
+        }catch(err) {
+            console.log(err);
+        } 
+    }
+render(){
+    return (
+        <>
+            <h2>{this.props.name}</h2>
+            <p>Class:{this.props.class}</p>
+            <p>Cost:{this.props.cost}</p>
+        </>
+    )
 }
+   
+    
+}
+
+
+
--- src/Starships.js
+++ src/Starships.js
@@ -1,16 +1,59 @@
 import React, { Component } from "react";
 import Starship from "./Starship";
+import axios from "axios";
 
 export default class Starships extends Component {
 
+
+
+    state = {
+        starships: []
+    }
+
+    componentDidMount = async () => {
+        try {
+            const response = await axios.get('https://swapi.dev/api/starships')
+            console.log('RESPONSE', response.data)
+            this.setState({
+                starships: response.data.results
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    componentDidUpdate() {
+        // setting state here can cause infinite loop
+        console.log('Starships.js did update')
+        //always will haveaccess to accurate state information
+        console.log('Starships.js current state', this.state)
+    }
+    //runs when the componet is unmounted - before next render but after state changed
+    componentWillUnmount() {
+        console.info('Starships.js is unmounting')
+    }
+
+
     render() {
-        const ships = this.props.starships.map((ship, i) => {
+        const shipsFromState = this.state.starships.map((ship, i) => {
             return (
                 <Starship
-                    key={`ship${i}`}
-                    ship={ship}
+                    key={`ship-${i}`}
+                    name={ship.name}
+                    class={ship.starship_class}
+                    cost={ship.cost_in_credits}
+                    pilots={ship.pilots}
                 />
             )
         })
+
+        return (
+          <>
+          <h1>Starships</h1>
+            {shipsFromState}
+          </>
+        )
     }
+
 }    
