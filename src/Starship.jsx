048294986d560dac800a48bb0c8148a1783940bb
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 11:13:23 -0500

    Add spacing and remove name label

--- src/Starship.jsx
+++ src/Starship.jsx
@@ -5,13 +5,13 @@ export default class Starship extends Component {
         console.log(this.props.starship)
         return (
             <div>
-                <h2>Name: {this.props.starship.name}</h2>
+                <h2>{this.props.starship.name}</h2>
                 <p>Class: {this.props.starship.starship_class}</p>
                 <p>Manufacturer: {this.props.starship.manufacturer}</p>
                 <p>Crew: {this.props.starship.crew}</p>
                 <p>Passengers: {this.props.starship.passengers}</p>
                 <p>Hyperdrive Rating: {this.props.starship.hyperdrive_rating}</p>
-
+                <br></br>
             </div>
         )
     }
