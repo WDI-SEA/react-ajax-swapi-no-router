c31d3068608b65cf92d92846f626dfe1fb5ba45c
JasonXChen <jasonxchen888@gmail.com>
2022-09-28 12:28:14 -0400

    Move starships state management to child component to prevent unnecessary future sibling component rendering

--- src/App.js
+++ src/App.js
@@ -1,60 +1,11 @@
 import React, { Component } from "react";
-import axios from "axios";
 import StarshipContainer from "./StarshipContainer";
 
 class App extends Component {
-  state = {
-    starshipArray: [],
-    page: 1
-  }
-  async componentDidMount() {
-    try {
-      const response = await axios.get("https://swapi.dev/api/starships?page=1");
-      this.setState({starshipArray: response.data.results});
-    }
-    catch (err) {
-      console.warn(err);
-    }
-  }
-  handlePaginationClick = async (button) => {
-    try {
-      let response = {};
-      switch (button) {
-        case "prev":
-          response = await axios.get(`https://swapi.dev/api/starships?page=${this.state.page - 1}`);
-          this.setState(prevState => {
-            return {
-              starshipArray: response.data.results,
-              page: prevState.page - 1
-            }
-          });
-          break;
-        case "next":
-          response = await axios.get(`https://swapi.dev/api/starships?page=${this.state.page + 1}`);
-          this.setState(prevState => {
-            return {
-              starshipArray: response.data.results,
-              page: prevState.page + 1
-            }
-          });
-          break;
-        default:
-          console.log("No matching pagination button");
-          break;
-      }
-    }
-    catch (err) {
-      console.warn(err);
-    }
-  }
   render() {
     return (
       <div className="m-5">
-        <StarshipContainer 
-          starshipArray={this.state.starshipArray}
-          page={this.state.page}
-          handlePaginationClick={this.handlePaginationClick}
-        />
+        <StarshipContainer />
       </div>
     );
   }
--- src/StarshipContainer.js
+++ src/StarshipContainer.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Starship from "./Starship";
 import PaginationButtons from "./PaginationButtons";
 
 class StarshipContainer extends Component {
+    state = {
+      starshipArray: [],
+      page: 1
+    }
+    async componentDidMount() {
+      try {
+        const response = await axios.get("https://swapi.dev/api/starships?page=1");
+        this.setState({starshipArray: response.data.results});
+      }
+      catch (err) {
+        console.warn(err);
+      }
+    }
+    handlePaginationClick = async (button) => {
+      try {
+        let response = {};
+        switch (button) {
+          case "prev":
+            response = await axios.get(`https://swapi.dev/api/starships?page=${this.state.page - 1}`);
+            this.setState(prevState => {
+              return {
+                starshipArray: response.data.results,
+                page: prevState.page - 1
+              }
+            });
+            break;
+          case "next":
+            response = await axios.get(`https://swapi.dev/api/starships?page=${this.state.page + 1}`);
+            this.setState(prevState => {
+              return {
+                starshipArray: response.data.results,
+                page: prevState.page + 1
+              }
+            });
+            break;
+          default:
+            console.log("No matching pagination button");
+            break;
+        }
+      }
+      catch (err) {
+        console.warn(err);
+      }
+    }
     render() {
-        const starshipComponents = this.props.starshipArray.map((starship, index) => {
+        const starshipComponents = this.state.starshipArray.map((starship, index) => {
             return (
                 <Starship 
                     starship={starship}
@@ -19,8 +64,8 @@ class StarshipContainer extends Component {
                     {starshipComponents}
                 </div>
                 <PaginationButtons 
-                    handlePaginationClick={this.props.handlePaginationClick}
-                    page={this.props.page}
+                    handlePaginationClick={this.handlePaginationClick}
+                    page={this.state.page}
                 />
             </div>
         );
