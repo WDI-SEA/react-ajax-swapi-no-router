3bb37d083eb5915a45fbd6e0579956d890486d24
fmher <feumatthewher2@gmail.com>
2023-01-17 10:03:20 -0800

    added bonus

--- src/Starship.jsx
+++ src/Starship.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 export default class Starship extends Component {
+
+    state = {
+        pilots: []
+    }
+
+    async componentDidMount() {
+        // fetch all of the pilots that are passed in from props
+        console.log(this.props.starship.pilots)
+        // map the array of urls to an array of pending promises
+        const pendingPromises = this.props.starship.pilots.map(url => axios.get(url))
+        console.log(pendingPromises)
+        // wait for all acios calls to respond using Promise.all
+        const responses = await Promise.all(pendingPromises)
+        console.log(responses)
+        // turn the arr of axiod responses into something that we can store in state and render
+        const responseDatas = responses.map((response) => response.data)
+        console.log(responseDatas)
+        // toss the data on pilot in state for later mapping 
+        this.setState({
+            pilots: responseDatas
+        })
+    }
+
     render() {
+        // map out the pilots
+        const pilots = this.state.pilots.map((pilot, i) => {
+            return (
+                <div key={(`pilot-${i}`)}>
+                    <p>Name: {pilot.name}, their mass: {pilot.mass}</p>
+                </div>
+            )
+
+        })
         return (
             <section>
                 {/* accessing data from starship from state arr, by using this.props.[obj within arr].[name of data that we will be extracting data] */}
@@ -9,6 +42,11 @@ export default class Starship extends Component {
                 <h3>{this.props.starship.class}</h3>
                 <p>by: {this.props.starship.manufacturer}</p>
                 <p>{this.props.starship.starship_class}</p>
+
+                <h3>Pilots</h3>
+
+                {pilots.length > 0 ? pilots : 'No Pilots Found'}
+
             </section>
         )
     }
