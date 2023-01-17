5417b219965268a4e8ccdefde6d5ba126a55b43f
nancyd333 <rgba0001@gmail.com>
2023-01-17 13:04:34 -0500

    update to include bonus

--- src/App.js
+++ src/App.js
@@ -1,77 +1,14 @@
 import React, {Component} from 'react';
-import axios from 'axios';
-import Starship from './Starship'
+import Starships from './Starships'
 
 export default class App extends Component {
 
-  state = {
-    starwarsApi: []
-  }
-
-  // use a componentDidMount if you need to display API data on page load
-  async componentDidMount() {
-    this.handleGetApi()
-
-    const options = {
-        headers: {
-            Accept: 'application/json'
-        }
-    }
-    const url = 'https://swapi.dev/api/starships'
-    try {
-        const response = await axios.get(url, options)
-        this.setState({
-            starwarsApi: response.data.results
-        })
-    } catch (err) {
-        console.log(err)
-    }
-  }
-
-  handleGetApi = async () => {
-      try {
-          const options = {
-              headers: {
-                  Accept: 'application/json'
-              }
-          }
-
-          const url = 'https://swapi.dev/api/starships'
-
-          const response = await axios.get(url, options)
-
-          this.setState({
-            starwarsApi: response.data
-          })
-          //console.log(this.state.starwarsApi)
-          //console.log(this.state.starwarsApi.next)
-          //console.log(this.state.starwarsApi[0].starship_class)
-      } catch (err) {
-          console.warn(err)
-      }
-  }
-
-  
-
-  
 
   render() {
   
-    const newarray = this.state.starwarsApi.map((ship, idx)=>{
-      return (
-        <Starship
-           shipName={ship.name}
-           shipClass={ship.starship_class}
-           key={`id-${idx}`}
-        />
-      )
-    })
-    return (
-    
-      <div>
-        <h1>StarShips</h1> 
-        {newarray}
-      </div>
+     return (
+     
+        <Starships />
       
     )
 }}
--- src/Starship.jsx
+++ src/Starship.jsx
@@ -1,22 +1,53 @@
+import axios from 'axios';
 import React, {Component} from 'react';
 
 
-export default class App extends Component {
+export default class Starship extends Component {
+  state = {
+    pilots: []
+  }
 
-   
+  async componentDidMount(){
+    
+    //console.log(this.props.starship.pilots)
+    
+    //map array of urls to an array of pending promises
+    const pendingPromises = this.props.starship.pilots.map(url=>axios.get(url))
+    // console.log(pendingPromises)
+    
+    //wait for all axioscalls to respond using Promise.all
+    const responses = await Promise.all(pendingPromises)
+    // console.log(responses)
+    
+    //turn the array of axios responses into something that we can store in state and render
+    const responseDatas = responses.map(response=>response.data)
+    // console.log(responseDatas)
+    
+    //toss the data on pilots in state for later mapping
+    this.setState({
+      pilots: responseDatas
+    })
+  }
 
   render() {
- 
-    // console.log("shipname", this.props.shipName)
+    //map out the pilots
+    const pilots = this.state.pilots.map((pilot, idx)=>
+        <div key={`pilot-${idx}`}>
+          <p>Name: {pilot.name}, this mass: {pilot.mass}</p>
+        </div>
+      )
     return (
      
-      <div>
-        <ul>
-            <li>ship: {this.props.shipName}</li>
-            <li>class: {this.props.shipClass}</li>
-        </ul>
- 
-      </div>
+      <section>
+
+            <h2>ship: {this.props.starship.name}</h2>
+            <h3>class: {this.props.starship.class}</h3>
+            <p>by: {this.props.starship.manufacturer}</p>
+            <p>{this.props.starship.starship_class} class vessel</p>
+
+            <h3>Pilots</h3>
+            {pilots.length>0 ? pilots : 'No Pilots Found'}
+      </section>
      )
   }
 }
