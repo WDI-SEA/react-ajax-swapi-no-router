5716dec2fca6cb3b90a807499c549fb11da1b754
juanedcabrera <juanedcabrera@gmail.com>
2023-04-20 19:24:11 -0600

    Final working Deliverable

--- src/AllStarships.jsx
+++ src/AllStarships.jsx
@@ -8,12 +8,30 @@ class AllStarships extends Component {
   }
 
 
-// componentDidMount with the axios
+componentDidMount() {
+    axios.get('https://swapi.dev/api/starships/')
+    .then(response => {
+        this.setState({ starships: response.data.results})
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
 
 render() {
+    const starshipComponents = this.state.starships.map(starship => { 
+        return <StarshipInfo key={starship.name} starship={starship}/>
+    }
+)
+console.log(starshipComponents)
+console.log(this.state.starships)
+
+
   return  (
     <div>
-      hello from the other side
+      <h2>Starwars Starships:</h2>
+      <p>{starshipComponents}</p>
+      
     </div>
     )
   }
--- src/StarshipInfo.jsx
+++ src/StarshipInfo.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
 
-const Starship = [] // needs more code here
+const Starship = ({starship}) => {
+    return (
+        <div>
+            <h2>{starship.name}</h2>
+            <p>Class: {starship.starship_class}</p>
+            <p>Hyperdrive Rating: {starship.hyperdrive_rating}</p>
+            <p>Megalight Per Hour: {starship.mglt}</p>
+            <p>Cost in Imperial Credits: {starship.cost_in_credits}</p>
+        </div>
+    )
+}
 
 export default Starship;
