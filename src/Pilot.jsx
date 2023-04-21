94121c54f2134693e7eeec1bd00a4e8def120c03
andrewbantly <andrewbantly@gmail.com>
2023-04-20 19:56:28 -0700

    incorporated pilots into Pilot component

--- src/Pilot.jsx
+++ src/Pilot.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios"
 
 export default class Pilot extends Component {
+    state = {
+        pilots: []
+    }
+    componentDidMount() {
+        const pilotList = [];
+        if (this.props.starship.pilots.length > 0) {
+        this.props.starship.pilots.map((pilot, i) => {
+                const pilotInfo = axios.get(pilot).then(response =>{
+                    console.log(response.data.name)
+                       pilotList.push(response.data.name)
+                          this.setState({
+                           pilots: pilotList
+                          })
+                })
+        })} else {
+            return (
+                "No Pilots"
+            )
+        }
+    }
     render(){
         return(
             <>
             <h5>
-                Pilots:
+                Pilots: {this.state.pilots}
                 </h5>
             </>
         )
